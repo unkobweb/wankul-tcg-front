5de85f4d6f14fda657d63e5a44706e7d9a127323
Alexandre Sieg <sieg.alexandre@gmail.com>
2023-10-01 18:36:28 +0200

    feat(header): improve header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,16 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Search } from 'lucide-react'
 import { Separator } from "./ui/separator";
+import {
+  Link,
+} from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <header className="bg-background flex flex-row items-center justify-between p-4 gap-6 sticky top-0 z-10">
-        <h1 className="font-bold">Wankul</h1>
+        <img src="/Wankul_Logo_Blanc.png" className="h-[30px] hidden dark:block" />
+        <img src="/Wankul_Logo_Noir.png" className="h-[30px] dark:hidden" />
         <div className="flex flex-row items-center w-[70%] max-w-[800px]">
           <Input className="mr-2" placeholder="Search..." />
           <Button variant="outline" size="icon">
@@ -16,11 +20,16 @@ const Header = () => {
           </Button>
         </div>
         <div className="flex flex-row items-center">
-          <Button variant="secondary" className="mr-2">Se connecter</Button>
+          <Link to="/login">
+            <Button variant="secondary" className="mr-2">Se connecter</Button>
+          </Link>
+          <Link to="/signup">
+            <Button variant="outline" className="mr-2">S'inscrire</Button>
+          </Link>
           <ModeToggle />
         </div>
       </header>
-      <Separator className="mb-6" />
+      <Separator />
     </>
   );
 };
